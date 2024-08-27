953ca0e2021e35b7b03058c2a021a0754daeab63
lzw <lzw@gmail.com>
2024-08-27 13:16:18 +0800

    imagecropper文件修改

--- src/App-imagecropper.vue
+++ src/App-imagecropper.vue
@@ -1,11 +1,23 @@
 <script lang="ts" setup>
 import imageCropper from './components/ImageCropper/ImageCropper.vue';
+import { ref } from 'vue'
+
+const dialogVisible = ref(false)
+const res = ref('')
+const finish = (data) => {
+  res.value = data
+  console.log(data, 'result')
+}
 
 </script>
 
 <template>
   <div class="app">
-    <imageCropper></imageCropper>
+    <el-button plain @click="dialogVisible = true">
+      打开裁剪工具
+    </el-button>
+    <imageCropper v-model:dialogVisible="dialogVisible" @finish="finish"></imageCropper>
+    <img :src="res" >
   </div>
 </template>
 
--- src/components/ImageCropper/ImageCropper.vue
+++ src/components/ImageCropper/ImageCropper.vue
@@ -1,40 +1,184 @@
 <script lang="ts" setup>
-import Cropper from 'cropperjs';
-import { onMounted, ref } from 'vue';
-const imageRef = ref()
-onMounted(() => {
-  if (imageRef.value) {
-    const cropper = new Cropper(imageRef.value, {
-      aspectRatio: 4 / 3, // 裁剪比例
-      viewMode: 0, // 裁剪模式
-      cropBoxMovable: true, // 允许移动裁剪框
-      cropBoxResizable: true, // 允许调整裁剪框大小
-      // zoomable: true, // 允许缩放
-      // zoomOnTouch: true, // 不允许触摸缩放
-      // zoomOnWheel: true, // 不允许鼠标滚轮缩放
-      // zoomOnDoubleClick: true, // 不允许双击缩放
-      // toggleDragModeOnDblclick: false, // 不允许双击切换拖拽模式
-      // responsive: true, // 响应窗口大小变化
-      autoCropArea: 0.5, // 自动裁剪区域的比例(裁剪框的默认宽高比)
-      // modal: true, // 取消遮罩层
-      background: false, // 取消背景
-      // guides: true, // 显示裁剪框内部的网格线
-      // dragMode: 'corp', // 拖动模式
-      rotatable: true, // 允许旋转
-    });
+import { reactive, ref, watch } from 'vue'
+import { ElMessageBox, ElMessage } from 'element-plus'
+import { DeleteFilled, Plus, Minus, RefreshLeft, RefreshRight } from '@element-plus/icons-vue'
+import 'vue-cropper/dist/index.css'
+import { VueCropper } from 'vue-cropper'
+interface imageCropperProps {
+  dialogVisible: boolean
+}
+interface imageCropperEmits {
+  (e: 'finish', value: any)
+  (e: 'update:dialogVisible', value: boolean)
+}
+const props = defineProps<imageCropperProps>()
+const emit = defineEmits<imageCropperEmits>()
+
+const cropper = ref()
+const dialogVisible = ref(props.dialogVisible)
+// const showCropper = ref(false)
+
+watch(() => props.dialogVisible, (newvalue) => {
+  dialogVisible.value = newvalue
+})
+const options = reactive({
+  img: null, // 裁剪图片的地址
+  autoCropWidth: 200, // 默认生成截图框宽度 默认容器的 80%
+  autoCropHeight: 200, // 默认生成截图框高度 默认容器的 80%
+  outputType: 'png', // 裁剪生成图片的格式 jpeg, png, webp
+  autoCrop: true, // 是否默认生成截图框
+  fixedBox: false, // 固定截图框大小
+  centerBox: true
+})
+const previews = ref({
+  url: ''
+})
+// 实时预览事件
+const realTime = (data) => {
+  previews.value = data
+  console.log(data, 'data')
+}
+
+// 修改图片大小 正数为变大 负数变小
+const changeScale = (num) => {
+  num = num || 1
+  cropper.value.changeScale(num)
+}
+// 向左边旋转90度
+const rotateLeft = () => {
+  cropper.value.rotateLeft()
+}
+// 向右边旋转90度
+const rotateRight = () => {
+  cropper.value.rotateRight()
+}
+// 上传图片处理
+const beforeUpload = (rawFile) => {
+  if (rawFile.type.indexOf('image/') == -1) {
+    ElMessage.error('请上传图片类型文件!')
+    return false
   }
-});
+  if (rawFile.size / 1024 / 1024 > 2) {
+    ElMessage.error('文件大小不能超过2MB!')
+    return false
+  }
+  const reader = new FileReader()
+  reader.readAsDataURL(rawFile)
+  reader.onload = () => {
+    // 图片在这里
+    console.log(reader.result)
+    options.img = reader.result
+  }
+}
+const reset = () => {
+    options.img = ''
+    previews.value.url = ''
+    // dialogVisible.value = false
+  }
+const finish = () => {
+  cropper.value.startCrop()
+  cropper.value.getCropData(data => {
+    console.log('data', data)
+    emit('update:dialogVisible', false)
+    emit('finish', data)
+    reset()
+  })
+}
+// 打开裁剪弹窗
+const openDialog = () => {
+  // showCropper.value = true
+}
+const handleClose = (done: () => void) => {
+  ElMessageBox.confirm('Are you sure to close this dialog?')
+    .then(() => {
+      done()
+    })
+    .catch(() => {
+      // catch error
+    })
+}
+
 </script>
 
 <template>
-<div class="imagecrapper">
-  <img  ref="imageRef" src="./3a47d64a0a3a48a28d22d8e4763b94b5.jpeg" alt="Picture">
+<div class="imagecropper">
+  <el-dialog
+    v-model="dialogVisible"
+    title="图片裁剪"
+    width="800"
+    :before-close="handleClose"
+    @opened="openDialog"
+  >
+    <el-row>
+      <el-col :span="12" style="height: 300px;">
+        <vue-cropper
+          ref="cropper"
+          :img="options.img"
+          :info="true"
+          :autoCrop="options.autoCrop"
+          :autoCropWidth="options.autoCropWidth"
+          :autoCropHeight="options.autoCropHeight"
+          :fixedBox="options.fixedBox"
+          :outputType="options.outputType"
+          :centerBox="options.centerBox"
+          @realTime="realTime"
+        />
+      </el-col>
+      <el-col :span="12" style="height: 300px;">
+        <div class="privew-box">
+          <img v-if="previews.url" :src="previews.url" :style="previews.img" />
+          <span v-else></span>
+        </div>
+      </el-col>
+    </el-row>
+    <el-row style="margin-top: 12px">
+      <el-col :span="12" >
+        <el-row>
+          <el-col :span="4" >
+            <el-upload
+              action="#"
+              :http-request="() => {}"
+              :before-upload="beforeUpload"
+              :show-file-list="false"
+            >
+              <el-button>选择</el-button>
+            </el-upload>
+          </el-col>
+          <el-col :span="4" style="text-align: right">
+            <el-button :icon="DeleteFilled" @click="reset"></el-button>
+          </el-col>
+          <el-col :span="4" style="text-align: right">
+            <el-button :icon="Plus" @click="changeScale(1)"></el-button>
+          </el-col>
+          <el-col :span="4" style="text-align: right">
+            <el-button :icon="Minus" @click="changeScale(-1)"></el-button>
+          </el-col>
+          <el-col :span="4" style="text-align: right">
+            <el-button :icon="RefreshLeft" @click="rotateLeft()"></el-button>
+          </el-col>
+          <el-col :span="4" style="text-align: right">
+            <el-button :icon="RefreshRight" @click="rotateRight()"></el-button>
+          </el-col>
+        </el-row>
+      </el-col>
+      <el-col :span="12" style="text-align: center">
+        <el-button type="primary" @click="finish">确认</el-button>
+      </el-col>
+    </el-row>
+  </el-dialog>
 </div>
 </template>
 
 <style scoped>
-  .imagecropper {
-  max-width: 400px;
-  max-height: 400px;
+.privew-box {
+  position: absolute;
+  left: 75%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  /* background: black; */
+  overflow: hidden;
 }
 </style>
